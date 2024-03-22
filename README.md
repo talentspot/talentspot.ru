288df54e9074174dba7b323a0a0566ae648fd4e8
Ашот Свазян <ashot.svazyan222@gmail.com>
2024-03-22 14:46:55 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+<p align="center">
+    <h1 align="center">Сайт ООО «ТалентСпот»</h1>
+</p>
---
+++ layouts/404.php
@@ -0,0 +1,32 @@
+<?php
+
+$title = 'Ошибка 404 — TalentSpot';
+$description = 'Платформа автоматизации процесса найма квалифицированного персонала';
+$name = 'ООО «ТалентСпот»';
+
+?>
+
+<?= HEAD($title, $name, $description) ?>
+
+<!-- MAIN CONTENT -->
+<main id="wrapper" class="wrapper">
+    <div class="global__error">
+
+        <div class="global__error-left">
+            <h1>Такой страницы нет</h1>
+            <span>Ошибка 404 :(</span>
+            <p>К сожалению, страница не найдена <br> Такой страницы не существует, либо она была не удалена</p>
+            <div>
+                <a href="/">TALENTSPOT</a>
+            </div>
+        </div>
+
+        <div class="global__error-right">
+            <img src="./img/404.png" alt="">
+        </div>
+
+    </div>
+</main>
+<!-- / MAIN CONTENT -->
+
+<?= FOOTER() ?>
